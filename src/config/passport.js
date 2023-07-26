ad3262cdd8a85f94b3f504911056a244696c5a3f
Correa7 <correadamian2019@gmail.com>
2023-07-26 14:35:20 -0300

    Practica integradora clase 21

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,8 +2,11 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const gitHubStrategy = require('passport-github2').Strategy
 const User = require('../dao/models/users.model')
+const Service = require('../routes/services/cart.services')
 const {createHash, isValidPass} = require ('../utils/bcrypt')
+const { githubClientId, githubSecret, githubCallBack} = require('../config/config')
 const fetch = require('node-fetch')
+const cartService= new Service()
  
 const initializePassport = () => {
 
@@ -34,6 +37,9 @@ const initializePassport = () => {
         {passReqToCallback:true, usernameField:'email'},
         async (req, username, password,done) => {
             try{
+                let data={}
+                let newCart = await cartService.postCart(data) 
+             
                 let userData = req.body
                 let userFound = await User.findOne({email:username})
                 if(userFound){
@@ -46,8 +52,9 @@ const initializePassport = () => {
                     email:userData.email,
                     age: userData.age || 25,
                     password:createHash(userData.password),
-                    rol: 'User'
-                }
+                    rol: 'User',
+                    cart:{_id:newCart._id}
+                } 
                 let result = await User.create(userNew)
                 done(null, result)
             }
@@ -58,9 +65,9 @@ const initializePassport = () => {
     ),
     passport.use('auth-github', new gitHubStrategy(
         {
-            clientID: '4612e26dbfd329ebc15e',
-            clientSecret: '4917423b1c6a348777c89f4b4f3af291e2dd5578',
-            callbackURL: "http://localhost:8080/auth/github/callback"
+            clientID: '4612e26dbfd329ebc15e', 
+            clientSecret:  '4917423b1c6a348777c89f4b4f3af291e2dd5578',
+            callbackURL: 'http://localhost:8080/auth/github/callback'
         },
         async (accessToken, refreshToken, profile, done)=>{
             try{
@@ -71,31 +78,37 @@ const initializePassport = () => {
                     'X-Github-Api-Version': '2022-11-28',
                 },
                 });
-
+                
                 const emails = await res.json();
                 const emailDetail = emails.find((email) => email.verified == true);
 
                 if (!emailDetail) {
                     return done(new Error('cannot get a valid email for this user'));
                 }
+                
+                let data={}
+                let newCart = await cartService.postCart(data) 
                 profile.email = emailDetail.email;
                 let user = await User.findOne({ email: profile.email });
-            if (!user) {
-                const newUser = {
-                first_name: profile._json.name || profile._json.login || 'noname',
-                last_name: profile._json.name || profile._json.login || 'no-last-name',
-                email:profile.email,
-                age: 18,
-                password:createHash('123'), 
-                rol: 'User'
-                };
-                let userCreated = await User.create(newUser);
-                console.log('User Registration succesful');
-                return done(null, userCreated);
-            } else {
-            console.log('User already exists');
-            return done(null, user);
-            }
+                
+                if (!user) {
+                    const newUser = {
+                    first_name: profile._json.name || profile._json.login || 'noname',
+                    last_name: profile._json.name || profile._json.login || 'no-last-name',
+                    email:profile.email,
+                    age: 18,
+                    password:createHash('123'), 
+                    rol: 'User',
+                    cart: {_id:newCart._id}
+                    };
+                    let userCreated = await User.create(newUser);
+                    console.log('User Registration succesful');
+                    return done(null, userCreated);
+                } else {
+                console.log('User already exists');
+                console.log('Esto client id es de github :' + githubClientId,' secret git :' + githubSecret)
+                return done(null, user);
+                }
             }
             catch(err) {
                 console.log('Error en auth github');
--- src/middlewares/middleware.auth.js
+++ src/middlewares/middleware.auth.js
@@ -2,14 +2,17 @@ function isUser(req, res, next) {
     if (req.session?.user?.email) {
       return next();
     }
-    return res.status(401).render('error', { error: 'error de autenticacion!' }); 
+    return res.status(401).render('error', { error: 'Error de autenticacion! Debes estar logueado para acceder a esta página.', style:'error404.css',
+    title:'Error de autenticacion' }); 
   }
   
   function isAdmin(req, res, next) {
-    if (req.session?.user?.rol) {
+    let session= req.session.user
+    if (req.session?.user?.rol === 'Admin') {
       return next();
     }
-    return res.status(403).render('error', { error: 'error de autorización!' });
+    return res.status(403).render('error', { error: 'Error de autorización!, Debes tener permisos de Administrador para acceder a esta página.', style:'error404.css',
+    title:'Error de autorización', session:session });
   }
 
   module.exports = { isUser,isAdmin}
